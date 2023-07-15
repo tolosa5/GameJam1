22c39d76aa8d202760846d9cfd391afcef648aa2
zomeze93 <zomeze93@icloud.com>
2023-07-15 03:46:41 +0200

    Hecho los enemigos y añadiendo una prueba de animacion de un enemigo

--- Assets/Ezequiel/Scripts/Enemy_MoveLeft.cs
+++ Assets/Ezequiel/Scripts/Enemy_MoveLeft.cs
@@ -2,21 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy_square : MonoBehaviour
+public class Enemy_MoveLeft : MonoBehaviour
 {
     private float _speed = 5f;
-    public bool touchBullet;
 
     void Update()
     {
         Vector2 newWay = new Vector2(transform.position.x - 1, transform.position.y);
         transform.position = Vector2.MoveTowards(transform.position, newWay, _speed * Time.deltaTime);
+    }
 
-        if (touchBullet)
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Bullet"))
         {
             Destroy(gameObject);
+            Destroy(collision.gameObject);
 
         }
     }
-
 }
--- Assets/Ezequiel/Scripts/Enemy_Proyecticles.cs
+++ Assets/Ezequiel/Scripts/Enemy_Proyecticles.cs
@@ -2,21 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy_square : MonoBehaviour
+public class Enemy_Proyecticles : MonoBehaviour
 {
     private float _speed = 5f;
-    public bool touchBullet;
+    private float _speedRotation = 1f;
+
 
     void Update()
     {
         Vector2 newWay = new Vector2(transform.position.x - 1, transform.position.y);
         transform.position = Vector2.MoveTowards(transform.position, newWay, _speed * Time.deltaTime);
-
-        if (touchBullet)
-        {
-            Destroy(gameObject);
-
-        }
     }
-
 }
