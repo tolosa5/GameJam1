9574397d87ac3dd2077270c73c2135b0e8876df7
tolosa5 <diegorinconesteban@gmail.com>
2023-07-15 16:27:55 +0200

    Disparos pa tras a medias y dash finiquitado

--- Assets/Diego/ScriptsDiego/Player.cs
+++ Assets/Diego/ScriptsDiego/Player.cs
@@ -32,7 +32,7 @@ public class Player : MonoBehaviour
     float lastShoot;
     [SerializeField] float fireRate;
 
-    [SerializeField] GameObject bullets; 
+    [SerializeField] GameObject bullets;
     [SerializeField] Sprite[] bulletsSprite; //cambiar el sprite de las balas segun el nivel en el que se esta
 
     #endregion
@@ -87,23 +87,9 @@ public class Player : MonoBehaviour
     private void Update()
     {
         h = Input.GetAxisRaw("Horizontal");
+
         CollisionDetection();
-        Debug.Log(dashCooldown);
-        switch(currentState)
-        {
-            case State.Fase1:
 
-            default:
-            case State.Fase2:
-                #region Dash
-                if (ableToDash && h != 0)
-                {
-                    StartCoroutine(Dash());
-                }
-                #endregion
-
-                break;
-        }
         #region JumpCall
 
         if (Input.GetKeyDown(KeyCode.Space))
@@ -120,15 +106,39 @@ public class Player : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.F) && lastShoot >= fireRate)
             Shoot();
 
-        if (Input.GetKeyDown(KeyCode.Q) && lastShoot >= fireRate)
-            BackShoot();
         #endregion
 
+        Debug.Log(dashCooldown);
+        switch(currentState)
+        {
+            case State.Fase1:
+
+            default:
+            case State.Fase2:
+                #region Dash
+                if (ableToDash && h != 0)
+                {
+                    StartCoroutine(Dash());
+                }
+                #endregion
+
+                #region BackFireCall
+
+                if (Input.GetKeyDown(KeyCode.Q) && lastShoot >= fireRate)
+                    BackShoot();
+                #endregion
+
+                break;
+        }
+
+
+        /*
         #region DashCall
 
         if (Input.GetKeyDown(KeyCode.LeftShift) && !isDashing)
             isDashing = true;
         #endregion
+        */
     }
 
     void Jump()
@@ -152,6 +162,8 @@ public class Player : MonoBehaviour
         yield return new WaitForSeconds(.2f);
         Instantiate(bullets, transform.position + Vector3.left * 1.5f, Quaternion.identity);
         lastShoot = 0;
+        yield return new WaitForSeconds(.2f);
+        sR.flipX = false;
     }
 
     IEnumerator Dash()
@@ -180,6 +192,6 @@ public class Player : MonoBehaviour
 
     public void Death()
     {
-
+        //si se mueve 11 bloques a la izquierda se ha muerto
     }
 }
